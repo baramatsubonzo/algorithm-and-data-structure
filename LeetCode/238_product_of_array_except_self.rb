fce4c3a62b2401a1bf16df44ca0c7b926beaed92
Shota Matsubara <baramatsubonzo@gmail.com>
2026-01-11 23:15:41 +0900

    238 product of array except self

--- LeetCode/238_product_of_array_except_self.rb
+++ LeetCode/238_product_of_array_except_self.rb
@@ -8,6 +8,29 @@ def product_except_self(nums)
   end
 end
 
+def product_except_self(nums)
+  n = nums.size
+  res = Array.new(n, 1)
+
+  # 1. 左側からの累積積を計算して入れる
+  # i番目には、「iより左にある数字の積」が入る
+  prefix = 1
+  (0...n).each do |i|
+    res[i] = prefix
+    prefix *= nums[i]
+  end
+
+  # 2. 右側からの累積積を「今の結果」にかけ合わせていく
+  # 後ろからループして、suffix（右側の積）を更新していく
+  suffix = 1
+  (n - 1).downto(0) do |i|
+    res[i] *= suffix
+    suffix *= nums[i]
+  end
+
+  res
+end
+
 # def product_except_self(nums)
 #   nums.each do |num|
 #     nums.slice!(num)
