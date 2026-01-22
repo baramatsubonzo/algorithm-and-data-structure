09d94a6c11f73ae2a49bf579bf7cc1d96b6ee3d8
Shota Matsubara <baramatsubonzo@gmail.com>
2026-01-22 12:15:01 +0900

    wip

---
+++ LeetCode/739_daily_temperatures.rb
@@ -0,0 +1,11 @@
+def daily_temperatures(temperatures)
+  stack = []
+  output = Array.new(temperatures.length, 0)
+
+  temperatures.each.with_index do |i, temerature|
+    stack << temperature
+    output[i] += 1
+    if temperature > 
+  end
+  output
+end
